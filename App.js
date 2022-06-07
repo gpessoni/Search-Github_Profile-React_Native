e127ba43e50a536e1e03b058f41a95b32d2dfaa0
gpessoni <pessonigs@gmail.com>
2022-06-07 08:18:56 -0300

    Update the App.js page (Main)

--- App.js
+++ App.js
@@ -6,11 +6,10 @@ import {
   SafeAreaView,
   StatusBar,
   Text,
-  Pressable,
+  TouchableOpacity,
   Linking,
   TextInput,
   Share,
-  
 } from "react-native";
 
 const App = () => {
@@ -56,62 +55,83 @@ const App = () => {
   const onShare = async () => {
     const result = await Share.share({
       message: `Veja o perfil de ${profile.name}  no link abaixo: ${profile.html_url}`,
-    })
+    });
+  };
 
-  }
+  const newSearch = async () => {
+    setProfile(null);
+  };
 
   return (
     <SafeAreaView style={style.container}>
       <StatusBar backgroundColor="#010409" barStyle="light-content" />
 
-      <View style={style.content}>
-        <Image source={require("./assets/logo.png")} style={style.logo} />
-        <TextInput
-          ref={inputRef}
-          style={style.input}
-          onChangeText={(text) => setUser(text)}
-          onSubmitEditing={handleRequestUserData}
-        />
-        <Pressable onPress={handleRequestUserData}>
-          <View style={style.button}>
-            <Text style={[style.defaultText, style.textButton]}>Buscar</Text>
-          </View>
-        </Pressable>
-      </View>
+      {profile === null && (
+        <View style={style.contentInput}>
+          <Image source={require("./assets/logo.png")} style={style.logo} />
+          <TextInput
+            ref={inputRef}
+            style={style.input}
+            onChangeText={(text) => setUser(text)}
+            onSubmitEditing={handleRequestUserData}
+          />
+          <TouchableOpacity onPress={handleRequestUserData}>
+            <View style={style.button}>
+              <Text style={[style.defaultText, style.textButton]}>Buscar</Text>
+            </View>
+          </TouchableOpacity>
+        </View>
+      )}
 
-      {profile && (
+      {profile !== null && (
         <View style={style.content}>
           <Image style={style.avatar} source={{ uri: profile?.avatar_url }} />
           <Text style={[style.textInfo, style.name]}>{profile?.name} </Text>
           <Text style={[style.textInfo, style.description]}>
-            USUÁRIO: {profile?.login}
+           <Text style={style.mainText}> USUÁRIO: </Text> {profile?.login}
+          </Text>
+          <Text style={[style.textInfo, style.bio]}>
+          <Text style={style.mainText}> BIO:{" "} </Text>
+            {profile?.bio !== null
+              ? profile?.bio.toUpperCase()
+              : "NÃO INFORMADO"}
           </Text>
           <Text style={[style.textInfo, style.description]}>
-            LOCALIZAÇÃO: {profile?.location !== null ? profile?.location.toUpperCase() : "Não informado"}
+          <Text style={style.mainText}> LOCALIZAÇÃO:{" "} </Text>
+            {profile?.location !== null
+              ? profile?.location.toUpperCase()
+              : "NÃO INFORMADO"}
           </Text>
           <Text style={[style.textInfo, style.description]}>
-            REPOSITÓRIOS: {profile?.public_repos}
+          <Text style={style.mainText}>  REPOSITÓRIOS:</Text> {profile?.public_repos} 
           </Text>
           <Text style={[style.textInfo, style.description]}>
-            SEGUIDORES: {profile?.followers}
+          <Text style={style.mainText}>  SEGUIDORES: </Text> {profile?.followers}
           </Text>
           <Text style={[style.textInfo, style.description]}>
-            SEGUINDO: {profile?.following}
+          <Text style={style.mainText}> SEGUINDO: </Text>{profile?.following}
           </Text>
-          <Pressable onPress={handlePressGoToGithub}>
+          <TouchableOpacity onPress={handlePressGoToGithub}>
             <View style={style.button}>
               <Text style={[style.defaultText, style.textButton]}>
                 ACESSAR GITHUB
               </Text>
             </View>
-          </Pressable>
-          <Pressable onPress={onShare}>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={onShare}>
             <View style={style.button}>
               <Text style={[style.defaultText, style.textButton]}>
                 COMPARTILHAR PERFIL
               </Text>
             </View>
-          </Pressable>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={newSearch}>
+            <View style={style.button}>
+              <Text style={[style.defaultText, style.textButton]}>
+                BUSCAR NOVAMENTE
+              </Text>
+            </View>
+          </TouchableOpacity>
         </View>
       )}
     </SafeAreaView>
@@ -122,13 +142,16 @@ export default App;
 
 const style = StyleSheet.create({
   container: {
-    // Column
     backgroundColor: "#010409",
-    flex: 1, // Expandir para a tela inteira
+    flex: 1, 
     alignItems: "center",
-    // flexDirection: 'row',
   },
-  content: {
+  contentInput: {
+    alignItems: "center",
+    padding: 20,
+    marginTop:'60%',
+  },
+    content: {
     alignItems: "center",
     padding: 20,
   },
@@ -140,10 +163,13 @@ const style = StyleSheet.create({
     borderWidth: 2,
   },
   defaultText: {
-    color: "#8a1a49",
+    color: "#66455f",
   },
   textInfo: {
     color: "#ffffff",
+  },
+  mainText:{
+    color: "#b596ae",
   },
   name: {
     marginTop: 20,
@@ -157,6 +183,16 @@ const style = StyleSheet.create({
   description: {
     fontWeight: "bold",
     fontSize: 14,
+    margin:8,
+  },
+  bio: {
+    fontWeight: "bold",
+    fontSize: 14,
+    textAlign: "center",
+    margin:8,
+  },
+  buttonMain: {
+    backgroundColor: "white",
   },
   button: {
     marginTop: 20,
@@ -179,6 +215,6 @@ const style = StyleSheet.create({
     marginBottom: 20,
   },
   logo: {
-   marginBottom: 20,
-  }
+    marginBottom: 20,
+  },
 });
